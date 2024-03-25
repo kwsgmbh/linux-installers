ca53a4f51b785c8a1f46c14b95786fa255c7dc50
Frank Bintakies <fbintakies@kws-computer.de>
2024-03-25 14:44:12 +0100

    BRBASE-35 first add of the linux-installer.sh script

---
+++ linux-installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update
+
+# Install desired packages
+sudo apt install -y apache2 mariadb-server mariadb-client php mosquitto build-essential git curl
+
+# Install wget
+wget https://dot.net/v1/dotnet-install.sh -O /home/kws/Downloads/dotnet-install.sh
+chmod +x /home/kws/Downloads/dotnet-install.sh
+/home/kws/Downloads/dotnet-install.sh --version latest --runtime aspnetcore
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+curl -sL https://install.raspap.com | bash
