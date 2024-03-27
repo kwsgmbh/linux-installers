d52cc0fe3f76ee47467fee5671802ddf6983fee6
Frank Bintakies <fbintakies@kws-computer.de>
2024-03-27 11:06:42 +0100

    BRBASE-35 remove of raspAP           add of jq for jeson decode

--- linux-installer.sh
+++ linux-installer.sh
@@ -4,11 +4,10 @@
 sudo apt update
 
 # Install desired packages
-sudo apt install -y apache2 mariadb-server mariadb-client php mosquitto build-essential git curl
+sudo apt install -y apache2 mariadb-server mariadb-client php mosquitto build-essential git curl jq
 
 # Install wget
 wget https://dot.net/v1/dotnet-install.sh -O /home/kws/Downloads/dotnet-install.sh
 chmod +x /home/kws/Downloads/dotnet-install.sh
 /home/kws/Downloads/dotnet-install.sh --version latest --runtime aspnetcore
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
-curl -sL https://install.raspap.com | bash
